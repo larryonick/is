9601eefaa1398cd8032b87dc8bd4fb64d5fab1f5
Lanre Fasuyi <lanrefasuyii@gmail.com>
2019-10-26 18:17:14 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+//Inventory sort configuration
+$CONFIG = '{"lang": "en", "error_reporting": false , "show_hidden": true, "hide_Cols": false, "calc_folder": false}';
+//IVS VERSION
+define('VERSION', '1.0.0');
+//title
+define('APP_TITLE', 'Inventory Sort');
+
+//Allow Authentication
+$use_auth = false;
+
+
+
+?>
